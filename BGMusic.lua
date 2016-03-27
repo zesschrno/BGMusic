71ffe9bbfb230d127edff1f3df1b059cd8b9aac5
zesschrno <kaoru.darkness@gmail.com>
2016-03-27 12:28:22 -0600

    Update BGMusic.lua

--- BGMusic.lua
+++ BGMusic.lua
@@ -1,1 +1,26 @@
+local get_loop_music = select(2, pcall(GetCVar, "Sound_ZoneMusicNoDelay"))
 
+local battle_music = CreateFrame("Frame")
+
+  battle_music:RegisterEvent("PLAYER_REGEN_DISABLED")
+  battle_music:RegisterEvent("PLAYER_REGEN_ENABLED")
+  
+  battle_music:SetScript("OnEvent", function(self, event, ...)
+    if(self[event]) then
+      self[event](self, event, ...)
+    end
+  end)
+  
+  function battle_music:PLAYER_REGEN_DISABLED(event)
+    if get_loop_music == "0" or get_loop_music == nil then
+      SetCVar("Sound_ZoneMusicNoDelay", "1")
+    end
+    PlayMusic("Interface\\AddOns\\BGMusic\\song.mp3");
+  end
+  
+  function battle_music:PLAYER_REGEN_ENABLED(event)
+    StopMusic();
+    if get_loop_music == "0" or get_loop_music == nil then
+      SetCVar("Sound_ZoneMusicNoDelay", "0")
+    end
+  end
