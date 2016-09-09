cbcf351b01e64a9307c9e24a07a9940e95e671fe
zesschrno <kaoru.darkness@gmail.com>
2016-09-09 18:42:54 -0500

    Update BGMusic.lua

--- BGMusic.lua
+++ BGMusic.lua
@@ -1,26 +1,22 @@
-local get_loop_music = select(2, pcall(GetCVar, "Sound_ZoneMusicNoDelay"))
+local player = nil
+local handler = nil
+local battle_music, battle_events = CreateFrame("Frame")
 
-local battle_music = CreateFrame("Frame")
+battle_events:RegisterEvent("PLAYER_REGEN_DISABLED")
+battle_events:RegisterEvent("PLAYER_REGEN_ENABLED")
 
-  battle_music:RegisterEvent("PLAYER_REGEN_DISABLED")
-  battle_music:RegisterEvent("PLAYER_REGEN_ENABLED")
-  
-  battle_music:SetScript("OnEvent", function(self, event, ...)
-    if(self[event]) then
-      self[event](self, event, ...)
-    end
-  end)
-  
-  function battle_music:PLAYER_REGEN_DISABLED(event)
-    if get_loop_music == "0" or get_loop_music == nil then
-      SetCVar("Sound_ZoneMusicNoDelay", "1")
-    end
-    PlayMusic("Interface\\AddOns\\BGMusic\\song.mp3");
-  end
-  
-  function battle_music:PLAYER_REGEN_ENABLED(event)
-    StopMusic();
-    if get_loop_music == "0" or get_loop_music == nil then
-      SetCVar("Sound_ZoneMusicNoDelay", "0")
-    end
-  end
+battle_music:SetScript("OnEvent", function(self, event, ...)
+  battle_events[event](self, ...)
+end)
+
+function battle_events:PLAYER_REGEN_DISABLED(event)
+  print("En batalla")
+  player, handler = PlaySoundFile("Interface\\AddOns\\BGMusic\\song.mp3")
+  print("Oyendo a balder")
+end
+
+function battle_events:PLAYER_REGEN_ENABLED(event)
+  print("Sin batalla")
+  StopMusic(handler)
+end
+
